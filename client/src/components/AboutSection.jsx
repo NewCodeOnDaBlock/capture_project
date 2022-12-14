51c29691119a692878952473a05eda9a0b8c3500
radenlmantuano <newcodeondablock@gmail.com>
2022-12-13 21:36:18 -0800

    devop: updated abou

--- client/src/components/AboutSection.jsx
+++ client/src/components/AboutSection.jsx
@@ -1,32 +1,65 @@
-import React from 'react'
-import home1 from '../assets/home1.png'
+import React from 'react';
+import home1 from '../assets/home1.png';
+import styled from 'styled-components';
+
 const AboutSection = () => {
   return (
-    <div className='about-section-container'>
-      <div className='description'>
+    <AboutSectionContainerStyled>
+      <DescriptionStyled>
         <div className='title'>
-          <div className='hide'>
+          <HideStyled>
             <h2>We work to make</h2>
-          </div>
-          <div className='hide'>
+          </HideStyled>
+          <HideStyled>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className='hide'>
+          </HideStyled>
+          <HideStyled>
             <h2>true.</h2>
-          </div>
+          </HideStyled>
         </div>
         <p>Contact us for any photography  or videography ideas that you have. 
           We have professionals with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div> 
-      <div className='image'>
+      </DescriptionStyled> 
+      <ImageStyled>
         <img src={home1} alt='imagePhoto'/>
-      </div> 
-    </div>
+      </ImageStyled> 
+    </AboutSectionContainerStyled>
   )
 }
+
+const AboutSectionContainerStyled = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const DescriptionStyled = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const ImageStyled = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const HideStyled = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
